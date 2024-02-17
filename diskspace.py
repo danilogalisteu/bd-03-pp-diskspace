2eb813db4ec945e39810cd33c65bafdd700e03b6
danilogalisteu <danilogalisteu@gmail.com>
2024-02-17 20:47:13 -0300

    added callbacks to select and scan folder

--- diskspace.py
+++ diskspace.py
@@ -1,18 +1,12 @@
 
 import pathlib
-import json
 
-from filesystem import DirEntry
 from window import Window
 
 
 
 if __name__ == '__main__':
     initial_path = pathlib.Path.home()
-
-    path_tree = DirEntry(initial_path).get_tree_info_r()
-
-    print(json.dumps(path_tree, sort_keys=True, indent=4))
 
     win = Window(1200, 900)
 
--- window.py
+++ window.py
@@ -29,17 +29,17 @@ class Window():
         self.select_entry = ttk.Entry(self.frame, textvariable=self.text_entry, width=50, state='readonly')
         self.select_entry.grid(column=1, row=0, padx=5, pady=10)
 
-        self.select_button = ttk.Button(self.frame, text="Select base folder", width=16)
+        self.select_button = ttk.Button(self.frame, text="Select base folder", width=16, command=self._select_folder)
         self.select_button.grid(column=2, row=0, padx=5, pady=10)
         self.select_button.focus()
 
-        self.scan_button = ttk.Button(self.frame, text="Scan folder", width=12)
+        self.scan_button = ttk.Button(self.frame, text="Scan folder", width=12, command=self._scan_folder)
         self.scan_button.grid(column=3, row=0, padx=5, pady=10)
 
         self.tree_view = ttk.Treeview(self.frame, height=20, selectmode='none', columns=['Size', 'Mode', 'Modified time'])
         self.tree_view.grid(column=0, row=1, columnspan=4, padx=10, pady=10)
 
-        self.frame.columnconfigure(3, weight=1)
+        self.frame.columnconfigure(1, weight=1)
         self.frame.rowconfigure(1, weight=1)
 
         self.frame.pack(fill="both", expand=True)
@@ -55,3 +55,18 @@ class Window():
 
     def close(self):
         self.running = False
+
+    def _select_folder(self):
+        base_path = filedialog.askdirectory(
+            parent=self.window,
+            title="Select base path",
+            initialdir=pathlib.Path.home(),
+            mustexist=True
+        )
+        if base_path:
+            self.base_path = pathlib.Path(base_path)
+            self.text_entry.set(str(self.base_path))
+
+    def _scan_folder(self):
+        self.dir_entry = DirEntry(self.base_path)
+        print(self.dir_entry.get_tree_info_r())
